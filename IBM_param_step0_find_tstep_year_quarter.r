1c6124fd7584ab7d41bb6d3569e56f34c4153d4b
Francois Bastardie <fba@aqua.dtu.dk>
2015-08-06 13:42:44 +0200

    add tsteps per start day for the calendar class

--- IBM_param_step0_find_tstep_year_quarter.r
+++ IBM_param_step0_find_tstep_year_quarter.r
@@ -1,12 +1,30 @@
 
 ## find the tstep corresponding to a change of year-quarter
 ## for a several years ahead
+
+general <- list()
+general$main.path.param   <- file.path("C:","Users","fbas","Documents","GitHub","DISPLACE_input_raw")
+
 start_y <- 2009
 end_y   <- 2015
 start_y <- 2012
 end_y   <- 2017
 t.seq <- seq(as.POSIXct(paste(start_y,"-01-01 00:00:00",sep='')),
         as.POSIXct(paste(end_y,"-12-31 00:00:00",sep='')), by="hours")
+
+
+#year-days
+idx <- NULL
+idx <- which( t.seq %in% as.POSIXct(paste(rep(start_y:end_y, each=365),"-",
+                                 c(rep('01',each=31), rep('02',each=28), rep('03',each=31), rep('04',each=30), 
+                                  rep('05',each=31), rep('06',each=30), rep('07',each=31), rep('08',each=31), rep('09',each=30), rep('10',each=31), rep('11',each=30), rep('12',each=31)),
+                                 "-", sprintf("%02d", c(1:31, 1:28,1:31, 1:30, 1:31, 1:30, 1:31, 1:31, 1:30, 1:31, 1:30, 1:31)),
+                                 " 00:00:00",sep='')))
+idx <- c(idx,   -1) # sentinel
+
+write.table(idx[-1], file= file.path(general$main.path.param, "simusspe", paste("tstep_days_",start_y,"_",end_y,".dat", sep="")),
+                      col.names=FALSE, row.names=FALSE)
+
 
 #year-months
 idx <- NULL
@@ -16,7 +34,7 @@ idx <- which( t.seq %in% as.POSIXct(paste(rep(start_y:end_y, each=12),"-",
                                         
 idx <- c(idx,   -1) # sentinel
 
-write.table(idx[-1], file= file.path("C:","Users","fba","Dropbox","ibm", "Cpp", "ibm_vessels", "simusspe", paste("tstep_months_",start_y,"_",end_y,".dat", sep="")),
+write.table(idx[-1], file= file.path(general$main.path.param, "simusspe", paste("tstep_months_",start_y,"_",end_y,".dat", sep="")),
                       col.names=FALSE, row.names=FALSE)
 
 
@@ -32,7 +50,7 @@ for(y in c(start_y:end_y)){
 }
 idx <- c(idx,   -1) # sentinel
 
-write.table(idx[-1], file= file.path("C:","Users","fba","Dropbox","ibm", "Cpp", "ibm_vessels", "simusspe", paste("tstep_quarters_",start_y,"_",end_y,".dat", sep="")),
+write.table(idx[-1], file= file.path(general$main.path.param, "simusspe", paste("tstep_quarters_",start_y,"_",end_y,".dat", sep="")),
                       col.names=FALSE, row.names=FALSE)
 
 #semesters
@@ -45,7 +63,7 @@ for(y in c(start_y:end_y)){
 idx <- c(idx,   -1) # sentinel
 
 
-write.table(idx[-1], file=file.path("C:","Users","fba","Dropbox","ibm", "Cpp", "ibm_vessels", "simusspe", paste("tstep_semesters_",start_y,"_",end_y,".dat", sep="")),
+write.table(idx[-1], file=file.path(general$main.path.param, "simusspe", paste("tstep_semesters_",start_y,"_",end_y,".dat", sep="")),
                       col.names=FALSE, row.names=FALSE)
 
 #years
@@ -56,5 +74,5 @@ for(y in c(start_y:end_y)){
 }
 idx <- c(idx,   -1) # sentinel
 
-write.table(idx[-1], file=file.path("C:","Users","fba","Dropbox","ibm", "Cpp", "ibm_vessels", "simusspe", paste("tstep_years_",start_y,"_",end_y,".dat", sep="")),
+write.table(idx[-1], file=file.path(general$main.path.param, "simusspe", paste("tstep_years_",start_y,"_",end_y,".dat", sep="")),
                       col.names=FALSE, row.names=FALSE)
