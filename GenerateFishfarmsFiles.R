ec03c91b3788ca1b2501c8f44749fe33ad6ee5d3
Francois Bastardie <fba@aqua.dtu.dk>
2016-09-29 09:52:33 +0200

    Adding base for R scripts on fishfarms and shipping

---
+++ GenerateFishfarmsFiles.R
@@ -0,0 +1,30 @@
+  # GENERAL SETTINGS
+
+   args <- commandArgs(trailingOnly = TRUE)
+
+   general <- list()
+
+   if (length(args) < 2) {
+     if(.Platform$OS.type == "windows") {
+       general$application           <- "balticRTI" # ...or myfish
+       general$main_path_gis         <- file.path("C:","Users","fbas","Documents","GitHub","DISPLACE_input_gis", general$application)
+       general$main.path.ibm         <- file.path("C:","Users","fbas","Documents","GitHub",paste("DISPLACE_input_", general$application, sep=''))
+       general$igraph                <- 56  # caution: should be consistent with existing objects already built upon a given graph
+       do_plot                       <- TRUE 
+     }
+  } else {
+       general$application           <- args[1]
+       general$main_path_gis         <- args[2]
+       general$main.path.ibm         <- args[3]
+       general$igraph                <- args[4]  # caution: should be consistent with existing objects already built upon a given graph
+       do_plot                       <- FALSE 
+  }
+  cat(paste("START\n"))
+  
+
+  dir.create(file.path(general$main.path.ibm, paste("fishfarmsspe_", general$application, sep='')))
+
+
+
+
+  cat(paste("..........done\n"))
