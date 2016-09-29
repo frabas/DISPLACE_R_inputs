aec1e10d106e7da6df6897db2905195bd32e1266
Francois Bastardie <fba@aqua.dtu.dk>
2016-09-29 09:40:28 +0200

    Fix Generate Harbour Files R routine

--- GenerateHarboursFiles.R
+++ GenerateHarboursFiles.R
@@ -7,16 +7,17 @@
    if (length(args) < 2) {
      if(.Platform$OS.type == "windows") {
        general$application           <- "balticRTI" # ...or myfish
-       general$main.path.param.gis   <- file.path("C:","Users","fbas","Documents","GitHub","DISPLACE_input_gis", general$application)
+       general$main_path_gis         <- file.path("C:","Users","fbas","Documents","GitHub","DISPLACE_input_gis", general$application)
        general$main.path.ibm         <- file.path("C:","Users","fbas","Documents","GitHub",paste("DISPLACE_input_", general$application, sep=''))
        general$igraph                <- 56  # caution: should be consistent with existing objects already built upon a given graph
-   
+       do_plot                       <- TRUE 
      }
   } else {
        general$application           <- args[1]
-       general$main.path.param.gis   <- args[2]
+       general$main_path_gis         <- args[2]
        general$main.path.ibm         <- args[3]
-       general$igraph                <- args[4]  # caution: should be consistent with existing vessels already built upon a given graph
+       general$igraph                <- args[4]  # caution: should be consistent with existing objects already built upon a given graph
+       do_plot                       <- FALSE 
   }
   
 
@@ -25,28 +26,29 @@
 
   ### build quick and dirty fake harbour files
 
-  port_names <- read.table(file=file.path(general$main.path.param.gis,  "GRAPH",
+  port_names <- read.table(file=file.path(general$main_path_gis,  "GRAPH",
                                   paste("harbours.dat", sep='')), sep=";")
+  cat(paste("Read port names...done\n"))
  
- # a quick check of consistent port_names file
- max_idx <- max(port_names$idx.port)
- c(1:max_idx)[ ! c(1:max_idx) %in% port_names$idx.port ]  # should return integer(0)
+  # a quick check of consistent port_names file
+  max_idx <- max(port_names$idx.port)
+  c(1:max_idx)[ ! c(1:max_idx) %in% port_names$idx.port ]  # should return integer(0)
 
 
  #load
-  coord <- read.table(file=file.path(general$main.path.param.gis, "GRAPH", paste("coord", general$igraph, ".dat", sep=""))) # build from the c++ gui
+  coord <- read.table(file=file.path(general$main_path_gis, "GRAPH", paste("coord", general$igraph, ".dat", sep=""))) # build from the c++ gui
   dd    <- coord
   coord <- as.matrix(as.vector(coord))
   coord <- matrix(coord, ncol=3)
   colnames(coord) <- c('x', 'y', 'harb')
-  plot(coord[,1], coord[,2])
+  if(do_plot) plot(coord[,1], coord[,2])
 
-
- 
-  graph <- read.table(file=file.path(general$main.path.param.gis, "GRAPH", paste("graph", general$igraph, ".dat", sep=""))) # build from the c++ gui
+  graph <- read.table(file=file.path(general$main_path_gis, "GRAPH", paste("graph", general$igraph, ".dat", sep=""))) # build from the c++ gui
   graph <- as.matrix(as.vector(graph))
   graph <- matrix(graph, ncol=3)
-  segments(coord[graph[,1]+1,1], coord[graph[,1]+1,2], coord[graph[,2]+1,1], coord[graph[,2]+1,2], col=4) # CAUTION: +1, because c++ to R
+  if(do_plot) segments(coord[graph[,1]+1,1], coord[graph[,1]+1,2], coord[graph[,2]+1,1], coord[graph[,2]+1,2], col=4) # CAUTION: +1, because c++ to R
+  
+  cat(paste("Read graph...done\n"))
 
 
 
@@ -60,15 +62,16 @@
   ##------------------------
   
   # stock names
-  spp_table <-  read.table(file=file.path(general$main.path.param.gis, "POPULATIONS", paste("pop_names_",general$application ,".txt",sep='')),
-              header=TRUE)
+  spp_table <-  read.table(file=file.path(general$main_path_gis, "POPULATIONS",
+                                    paste("pop_names_",general$application,".txt",sep='')), header=TRUE)
   spp                        <- as.character(spp_table$spp)
 
-  
+  cat(paste("Read stock names...done\n"))
+ 
   
   ##------------------------
   # obtain a c++ multimap with stock / price for cat
-  prices_per_szcat <-  read.table(file=file.path(general$main.path.param.gis, "POPULATIONS", paste("Stock_prices_data.csv")), sep=';',
+  prices_per_szcat <-  read.table(file=file.path(general$main_path_gis, "POPULATIONS", paste("Stock_prices_data.csv")), sep=';',
               header=TRUE)
   #head(prices_per_szcat)
   #stock small medium large  
@@ -78,6 +81,8 @@
  #FLE.2425   0.5      1     2 
  #PLE.2123   0.5      1     2 
  #PLE.2432   0.5      1     2
+ cat(paste("Read stock prices per commercial category...done\n"))
+
 
 for (i in idx){
  prices_per_species_per_cat <- prices_per_szcat [, 2:4]  # price in euro per kilo for three size (small, medium, and large) 
@@ -95,9 +100,15 @@ for (i in idx){
  write.table(cbind(stock= rep(0:(length(spp)-1), each=3), price_per_cat= c(t(as.matrix(prices_per_species_per_cat))) ), 
                 file= file.path(general$main.path.ibm, paste("harboursspe_", general$application, sep=''), paste(i,"_quarter4_each_species_per_cat.dat",sep="")), row.names=FALSE, quote=FALSE)
  
+ cat(paste("Write ", i,"_quarterXX_each_species_per_cat.dat in /harboursspe...done\n"))
+
 }
                                  
 
  write.table(cbind(node=idx,  name=sapply(rownames(port_names), function (x) paste(unlist(strsplit(x, " ")), collapse="_"))    ),
     file= file.path(general$main.path.ibm, paste("harboursspe_", general$application, sep=''),  "names_harbours.dat"), row.names=FALSE, col.names=TRUE, quote=FALSE)
 
+ cat(paste("Write names_harbours.dat in /harboursspe...done\n"))
+
+
+  cat(paste("..........done\n"))
