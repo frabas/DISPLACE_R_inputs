76cc0214fa1e5938b712c9cb3a223ec301f566e4
Francois Bastardie <fba@aqua.dtu.dk>
2016-10-05 13:48:44 +0200

    Add entry points for benthos specs from the Objects Editor

--- GenerateBenthosLandscapeOnNodes.R
+++ GenerateBenthosLandscapeOnNodes.R
@@ -8,7 +8,7 @@
      if(.Platform$OS.type == "windows") {
        general$application           <- "balticRTI" # ...or myfish
        general$main_path_gis         <- file.path("C:","Users","fbas","Documents","GitHub","DISPLACE_input_gis", general$application)
-       general$main.path.ibm         <- file.path("C:","Users","fbas","Documents","GitHub", paste("DISPLACE_input_", general$application,"_ok", sep=''))
+       general$main.path.ibm         <- file.path("C:","Users","fbas","Documents","GitHub", paste("DISPLACE_input_", general$application, sep=''))
        general$igraph                <- 56  # caution: should be consistent with existing objects already built upon a given graph
        do_plot                       <- TRUE
      }
